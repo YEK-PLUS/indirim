2dfc2407897370b85f3e1cf0eb61260d448746c6
YEK-PLUS <yek@f4med.com>
2019-07-10 18:12:47 +0300

    changing printSvg to get svg

--- src/functions.php
+++ src/functions.php
@@ -1,7 +1,7 @@
 <?php
-function printSVG($name){
+function getSVG($name){
   if(file_exists(DIR."/asset/svg/".$name.".svg")){
-    echo file_get_contents(DIR."/asset/svg/".$name.".svg");
+    return file_get_contents(DIR."/asset/svg/".$name.".svg");
   }
 }
 function setParams($params,$key){
