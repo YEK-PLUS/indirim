9463c53efb354753e527356b81c7a6d241f21385
YEK-PLUS <yek@f4med.com>
2019-07-10 18:18:41 +0300

    adding one line for justify

--- src/methods/meta.php
+++ src/methods/meta.php
@@ -1,5 +1,6 @@
 <?php
 $this->addMethod("meta",function(){
+  echo "\n";
   echo <<<HTML
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
